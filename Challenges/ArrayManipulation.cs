f2af91e8ca389086a70ae3db38ea3611e246d7df
djangojazz <brettmorin2@gmail.com>
2025-04-15 14:40:30 -0700

    Updated more questions

---
+++ Challenges/ArrayManipulation.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenges
+{
+    internal class ArrayManipulation
+    {
+        internal static long arrayManipulation(int n, List<List<int>> queries)
+        {
+            //Create a list of n elements
+            var list = new List<int>(new int[n]);
+
+            //Iteration of each set
+            for (int i = 0; i < queries.Count; i++)
+            {
+                var (a, b, k) = (queries[i][0], queries[i][1], queries[i][2]);
+
+                //Iteration of each value in set
+                for (int j = 1; j <= n; j++)
+                {
+                    //If the value is in the range of a and b, add k to it
+                    if (j >= a && j <= b)                    
+                        list[j - 1] += k;  //Update the list at the associated index
+                }
+
+            }
+
+
+
+            return list.OrderByDescending(x => x).FirstOrDefault();
+        }
+
+        internal static string WriteLineOfNArray(IEnumerable<int> list) => string.Join(", ", list);
+
+        internal static void Run(int n, List<List<int>> queries)
+        {
+            long result = arrayManipulation(n, queries);
+
+            Console.WriteLine(result);
+        }
+    }
+}
--- Challenges/Program.cs
+++ Challenges/Program.cs
@@ -4,5 +4,14 @@
 //Larrys_Array.Run(null);
 //InsertDataAtTail.Run();
 //InsertDataAtHead.Run();
+//SparseArrays.Run(new List<string> { "aba", "baba", "aba", "xzxb" }, new List<string> { "aba", "xzxb", "ab" });
 
-SparseArrays.Run(new List<string> { "aba", "baba", "aba", "xzxb" }, new List<string> { "aba", "xzxb", "ab" });
+int n = 5;
+var queries = new List<List<int>>()
+{
+    new List<int>() { 1, 2, 100 },
+    new List<int>() { 2, 5, 100 },
+    new List<int>() { 3, 4, 100 },
+    new List<int>() { 1, 1, 200 }
+};
+ArrayManipulation.Run(n, queries);
