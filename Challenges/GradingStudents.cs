e7064be681a37916ce51da06987ce22434156655
djangojazz <brettmorin2@gmail.com>
2025-04-16 13:31:33 -0700

    Updated Grading students

---
+++ Challenges/GradingStudents.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenges
+{
+    internal class GradingStudents
+    {
+        internal static List<int> gradingStudents(List<int> grades) =>
+            grades.Select(x => {
+
+                    if (x >= 38)
+                    {
+                        var div = (int)(x / 5);
+                        var next = 5 * (div + 1);
+                        var diff = next - x;
+                        x = diff < 3 ? next : x;
+                    }
+
+                    return x;
+                }).ToList();
+
+        internal static void Run(List<int> grades)
+        {
+            int gradesCount = grades.Count;
+
+
+            for (int i = 0; i<gradesCount; i++)
+            {
+                int gradesItem = Convert.ToInt32(Console.ReadLine().Trim());
+                grades.Add(gradesItem);
+            }
+
+            List<int> result = gradingStudents(grades);
+
+            Console.WriteLine(String.Join("\n", result));
+        }
+
+    }
+}
