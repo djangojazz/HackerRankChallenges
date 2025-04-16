057d5e0844ec66fd7fb0ebc359626608bc1eabce
djangojazz <brettmorin2@gmail.com>
2025-04-15 17:12:59 -0700

    Updated for more answers

---
+++ Challenges/LinkedListsRemovingDupes.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenges
+{
+    internal class LinkedListsRemovingDupes
+    {
+        class SinglyLinkedListNode
+        {
+            public int data;
+            public SinglyLinkedListNode next;
+
+            public SinglyLinkedListNode(int nodeData)
+            {
+                this.data = nodeData;
+                this.next = null;
+            }
+        }
+
+        class SinglyLinkedList
+        {
+            public SinglyLinkedListNode head;
+            public SinglyLinkedListNode tail;
+
+            public SinglyLinkedList()
+            {
+                this.head = null;
+                this.tail = null;
+            }
+
+            public void InsertNode(int nodeData)
+            {
+                SinglyLinkedListNode node = new SinglyLinkedListNode(nodeData);
+
+                if (this.head == null)
+                {
+                    this.head = node;
+                }
+                else
+                {
+                    this.tail.next = node;
+                }
+
+                this.tail = node;
+            }
+        }
+
+        static void PrintSinglyLinkedList(SinglyLinkedListNode node)
+        {
+            while (node != null)
+            {
+                Console.WriteLine(node.data);
+
+                node = node.next;
+            }
+        }
+
+        static SinglyLinkedListNode removeDuplicates(SinglyLinkedListNode llist, int prev = 0)
+        {
+            if (llist?.data == null)
+                return new SinglyLinkedListNode(0);
+
+            if (llist?.next != null)
+            {
+
+                //pop the dupe as you go by making the next skipping to the next one.
+                if (prev == llist.data && llist.next != null)
+                {
+                    llist.data = llist.next.data;
+                }
+
+                
+                if (llist.data == llist.next.data)
+                {
+                    llist.next = llist?.next?.next ?? null;
+                }
+
+                removeDuplicates(llist.next, llist.data);
+            }
+
+            return llist;
+        }
+
+        internal static void Run(int t, int n, params int[] ints)
+        {
+            for (int tItr = 0; tItr < t; tItr++)
+            {
+                SinglyLinkedList llist = new SinglyLinkedList();
+
+
+                for (int i = 0; i < n; i++)
+                {
+                    llist.InsertNode(ints[i]);
+                }
+
+                SinglyLinkedListNode llist1 = removeDuplicates(llist.head);
+
+                PrintSinglyLinkedList(llist1);
+            }
+        }
+    }
+}
--- Challenges/Program.cs
+++ Challenges/Program.cs
@@ -6,12 +6,21 @@
 //InsertDataAtHead.Run();
 //SparseArrays.Run(new List<string> { "aba", "baba", "aba", "xzxb" }, new List<string> { "aba", "xzxb", "ab" });
 
+//int n = 5;
+//var queries = new List<List<int>>()
+//{
+//    new List<int>() { 1, 2, 100 },
+//    new List<int>() { 2, 5, 100 },
+//    new List<int>() { 3, 4, 100 },
+//    new List<int>() { 1, 1, 200 }
+//};
+//ArrayManipulation.Run(n, queries);
+
+int t = 1;
 int n = 5;
-var queries = new List<List<int>>()
-{
-    new List<int>() { 1, 2, 100 },
-    new List<int>() { 2, 5, 100 },
-    new List<int>() { 3, 4, 100 },
-    new List<int>() { 1, 1, 200 }
-};
-ArrayManipulation.Run(n, queries);
+int[] m = new int[] { 1, 2, 2, 3, 4 };
+
+int t2 = 1;
+int n2 = 6;
+int[] m2 = new int[] { 3, 3, 3, 4, 5, 5 };
+LinkedListsRemovingDupes.Run(t2, n2, m2);
